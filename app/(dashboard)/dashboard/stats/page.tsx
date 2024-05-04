88413451053169c16e7ecdf2f3342980fcf5bd16
shaheen <84270559+shaheen-senpai@users.noreply.github.com>
2024-05-04 06:47:14 +0530

    graph stat

--- app/(dashboard)/dashboard/stats/page.tsx
+++ app/(dashboard)/dashboard/stats/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { useState, useEffect } from "react"
-import { getMoodRecords } from "@/lib/supabase/helpers"
+import { getMoodRecords, getMoodRecordsForGraph } from "@/lib/supabase/helpers"
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
 
 export default function Page() {
 
     const [stats, setStats] = useState<any>([])
+    const [graph, setGraphData] = useState<any>([])
 
     useEffect(()=> {
         const fetchData = async () => {
@@ -15,10 +18,18 @@ export default function Page() {
         }
         fetchData()
 
+        const fetchGrpah = async () => {
+            const graphData = await getMoodRecordsForGraph()
+            console.log(graphData)
+            setGraphData(graphData)
+        }
+        fetchGrpah()
+
     }, [])
 
     return (
         <div>
+
             <h1>Stats</h1>
             <table>
                 <thead>
@@ -38,6 +49,25 @@ export default function Page() {
                     ))}
                 </tbody>
             </table>
+
+            <h1>Graph</h1>
+            <LineChart
+                width={600}
+                height={300}
+                data={graph}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+            >
+                <XAxis dataKey="created_at" />
+                <YAxis />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="mood_score" stroke="#8884d8" activeDot={{ r: 8 }} />
+            </LineChart>
+
         </div>
     )
+
+
+
 }
